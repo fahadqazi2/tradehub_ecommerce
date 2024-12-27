db7c0b335347ac4d5593e6772e2f160fafc2b529
fahadqazi2 <131265038+fahadqazi2@users.noreply.github.com>
2024-12-27 21:11:24 +0530

    Add files via upload

---
+++ admin_area/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin dashboard</title>
+    <!-- bootstrap css link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- css file -->
+    <link rel="stylesheet" href="../style.css">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <style>
+        .admin_image{
+    width: 100px;
+    object-fit: contain;
+
+}
+    </style>
+</head>
+<body>
+    <!-- navbar -->
+    <dic class="container-fluid p-0">
+        <!-- first child -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <div class="container-fluid">
+                <img src="../pics/logo2.jpg" alt="" class="nav-logo">
+            </div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-info">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Welcome Guest</a>
+                    </li>
+                </ul>
+            </nav>
+        </nav>
+
+        <!-- second child -->
+        <div class="bg-light">
+            <h3 class="text-center p-2">Manage Details</h3>
+        </div>
+
+        <!-- third child -->
+        <div class="row">
+            <div class="col-md-12 bg-secondary p-1 d-flex align-items-center">
+                <div class="p-3">
+                    <a href="#"><img src="../pics/carrot1.jpg" alt="" class="admin_image"></a>
+                    <p class="text-light text-center">Admin Name</p>
+                </div>
+                <div class="button text-center">
+                    <button><a href="insert_product.php" class="nav-link text-light bg-info my-1">Insert products</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View products</a></button>
+                    <button><a href="index.php?insert_categories" class="nav-link text-light bg-info my-1">Insert catogeries</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View catogeries</a></button>
+                    <button><a href="index.php?insert_brands" class="nav-link text-light bg-info my-1">Insert brands</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">View brands</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">All orders</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">All payments</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">List users</a></button>
+                    <button><a href="" class="nav-link text-light bg-info my-1">Logout</a></button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- fourth child -->
+    <div class="container my-3">
+        <?php
+        if(isset($_GET['insert_categories'])){
+            include('insert_categories.php');
+        }
+        if(isset($_GET['insert_brands'])){
+            include('insert_brands.php');
+        }
+        ?>
+    </div>
+<!--last child-->
+<div class="bg-info p-3 text-center footer" style="width: 100%;">
+    <p>All rights reserved <i class="fa-regular fa-copyright"></i>- designed by Fahad Qazi</p>
+</div>
+
+
+<!-- bootstrap js link -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
